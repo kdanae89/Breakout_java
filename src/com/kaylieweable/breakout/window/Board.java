23c1f7e0f72ae81612e07ad75d0d1365ebd1712e
Kaylie Weable <kayliedanae@hotmail.com>
2017-04-22 17:19:26 -0700

    learning how to configure git and eclipse

---
+++ src/com/kaylieweable/breakout/window/Board.java
@@ -0,0 +1,45 @@
+package com.kaylieweable.breakout.window;
+//allows access to JPanel library
+import javax.swing.JPanel;
+//extends gives Board access to JPanel
+public class Board extends JPanel implements Runnable{
+//private sets variables specific to Board class
+	private boolean running = false;
+	private Thread thread;
+	//only one instance of width an height --> static
+	private static int WIDTH, HEIGHT;
+	
+	public Board(){
+		
+	}
+	
+	private void init(){
+		//focuses on board; without, game pieces would not move --> built into JPanel
+		setFocusable(true);
+		//built into JPanel as well
+		WIDTH = getWidth();
+		HEIGHT = getHeight();
+	}
+	
+	public synchronized void start(){
+		//concurrency = allowance of multiple activities at one time
+		//establishes variable changes for all other threads
+		if(running){
+			return;
+		}
+		running = true;
+		thread = new Thread(this);
+		//public void run will not happen without
+		thread.start();
+	}
+	//primary method for our application - will run whenever a thread is started
+	public void run() {
+		//call to private method init
+		init();
+		
+		while(running){
+			//game loop
+		}
+	}
+}
+
---
+++ src/com/kaylieweable/breakout/window/Window.java
@@ -0,0 +1,43 @@
+package com.kaylieweable.breakout.window;
+//imports Dimension library
+import java.awt.Dimension;
+//imports JFrame library
+import javax.swing.JFrame;
+
+public class Window {
+//public available outside of class
+//	this is a constructor vvv(always public)
+	//default constructor
+	public Window(int w, int h, String title, Board board){
+		//code runs immediately as an object of our class is created
+		board.setPreferredSize(new Dimension(w,h));
+		board.setMaximumSize(new Dimension(w,h));
+		board.setMinimumSize(new Dimension(w,h));
+		//houses and contains things like JPanels - it's a top level container for swing GUIs
+		//Swing GUI cannot exist without a top level container
+		JFrame frame = new JFrame(title);
+		//adds board to JFrame
+		frame.add(board);
+		//setting frame dimensions = to board
+		frame.pack();
+		//jframe dissapear once we exit
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//dont want our frame to resize
+		frame.setResizable(false);
+		//want our board in the middle of our screen
+		frame.setLocationRelativeTo(null);
+		//want our board to show up duh?
+		frame.setVisible(true);
+		
+		board.start();
+	}
+//	needed to run app unless applet
+//	always in window.java
+	public static void main(String[] args){
+		//this would create a variable if we were to create more than one, same with board
+		//Window window = new Window(800, 600, "Breakout!", new Board());
+		
+		
+		
+	}	
+}
